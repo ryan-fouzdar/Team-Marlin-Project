8598c137ccbc320b1439a3e690fbdd6986540b67
nayoseph <nayoseph@ucdavis.edu>
2024-02-01 10:49:19 -0800

    added to homepage

--- flutter_application_1/lib/homepage.dart
+++ flutter_application_1/lib/homepage.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // home: const LoginPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
