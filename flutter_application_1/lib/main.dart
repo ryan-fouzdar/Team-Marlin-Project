5f0a6750b0c16b2c8bd62e8225ef37b0802dae73
Ryan Fouzdar <ryanfouzdar@gmail.com>
2024-01-31 18:39:35 -0800

    Finished to login screeen

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -31,43 +31,78 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+       home
+       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+  class loginPage extends StatelessWidget{
+    const loginPage({Key? key, required this.title}) : super(key: key);
+    final String title;
+    @override
+    Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(Login to AggieBites!)
+        body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            const SizedBox(height: 16),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
+  // class MyHomePage extends StatefulWidget {
+  //   const MyHomePage({super.key, required this.title});
 
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
+  //   // This widget is the home page of your application. It is stateful, meaning
+  //   // that it has a State object (defined below) that contains fields that affect
+  //   // how it looks.
 
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
+  //   // This class is the configuration for the state. It holds the values (in this
+  //   // case the title) provided by the parent (in this case the App widget) and
+  //   // used by the build method of the State. Fields in a Widget subclass are
+  //   // always marked "final".
 
-//   final String title;
+  //   final String title;
 
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+  //   @override
+  //   State<MyHomePage> createState() => _MyHomePageState();
+  // }
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
+  // class _MyHomePageState extends State<MyHomePage> {
+  //   int _counter = 0;
 
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
+  //   void _incrementCounter() {
+  //     setState(() {
+  //       // This call to setState tells the Flutter framework that something has
+  //       // changed in this State, which causes it to rerun the build method below
+  //       // so that the display can reflect the updated values. If we changed
+  //       // _counter without calling setState(), then the build method would not be
+  //       // called again, and so nothing would appear to happen.
+  //       _counter++;
+  //     });
+  //   }
 
   // @override
   // Widget build(BuildContext context) {
