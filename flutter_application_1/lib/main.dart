fb727bc727c64a79c53e515f098c13d7380d53b2
nayoseph <nayoseph@ucdavis.edu>
2024-02-28 18:31:38 -0800

    changes to dashboard, added to mealSelection

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
-// import 'pages/homepage.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,43 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-class LoginPage extends StatelessWidget{
-  const LoginPage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login to AggieBites!")
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-              ),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Login'),
-            ),
-          ],
-        ),
-      ),
+      home: const LoginPage(title: "Login to AggieBites!"),
     );
   }
 }
--- flutter_application_1/lib/pages/login_page.dart
+++ flutter_application_1/lib/pages/login_page.dart
@@ -8,7 +8,7 @@ class LoginPage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Login to AggieBites!")
+        title: Text(title)
       ),
       body: Center(
         child: Column(
