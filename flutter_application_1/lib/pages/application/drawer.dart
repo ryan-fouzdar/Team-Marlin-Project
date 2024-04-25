c1d6b54567cf52b31f2dc2d7f5fa78b079611ed2
nayoseph <nayoseph@ucdavis.edu>
2024-04-24 18:23:39 -0700

    more refactoring

--- flutter_application_1/lib/pages/application/drawer.dart
+++ flutter_application_1/lib/pages/application/drawer.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/login_page.dart';
-import 'meal_selection.dart'; // Ensure this import points to the correct file path
+import 'package:flutter_application_1/pages/authentication/login_page.dart';
+import '../profile_creation/meal_selection.dart'; // Ensure this import points to the correct file path
 import 'dashboard.dart';
 import 'meal_page.dart';
-import 'login_page.dart';
+import '../authentication/login_page.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
