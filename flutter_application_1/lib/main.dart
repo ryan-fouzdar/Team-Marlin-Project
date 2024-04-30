331d6543728eb8a684ea8e5df2b8cb73f5875b2b
Ryan Fouzdar <ryanfouzdar@gmail.com>
2024-04-30 10:57:38 -0700

    cache changes

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
+import 'pages/authentication/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'misc/firebase_options.dart';
 
 
 void main() async {
